11e1fc41cafc00731b90b205d91eb48c3f150988
dirgni <inx116@hotmail.com>
2013-03-14 02:48:11 +0200

    Sort

--- war/main.js
+++ war/main.js
@@ -1,3 +1,25 @@
 
 $('.menu').tabify();
 
+//$(function() {
+//    var available_partei = [
+//      "VOT",
+//      "Kuusekesed",
+//      "Puukesed",
+//      "Kasekesed",
+//      "Randomness"
+//    ];
+//    
+//    $( "#nimiser" ).autocomplete({
+//      source: available_partei
+//    });
+//  });
+
+//function signingout(){
+//	var linkTag = document.createElement("link");
+//	linkTag.href = "kylaline.css";
+//	linkTag.rel = "stylesheet";
+//	var head = document.getElementsByTagName ("head")[0];
+//	head.appendChild (linkTag);
+//}
+
