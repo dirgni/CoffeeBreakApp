ef1886b321d7a3084c10570f6d41dfbdb2ce45f4
a71993 <a71993@ut.ee>
2013-03-14 02:10:08 +0200

    kandidaatide json tulemuste tabelisse, töötab chromes a mitte IE-s

---
+++ war/table2.js
@@ -0,0 +1,45 @@
+//$.getJSON("kandidaadid.json" , function(data) {
+//    var tbl_body = "";
+//    $.each(data, function() {
+//        var tbl_row = "";
+//        $.each(this, function(k , v) {
+//            tbl_row += "<td>"+v+"</td>";
+//        })
+//        tbl_body += "<tr>"+tbl_row+"</tr>";                 
+//    })
+//    $("#mytable tbody").html(tbl_body);
+//});
+//var json = [{"member_id":"2","comment":"kkk"},{"member_id":"1","comment":"this is admin 2"},{"member_id":"2","comment":"kkk"},{"member_id":"1","comment":"this is admin"}];
+//$.each(json, function(i, data){
+//$("table.table").append("<tr><td>" + data.member_id + "</td><td>" + data.comment + "</td></tr>");
+//})    
+//$.ajax({
+//    url: 'kandidaadid.json',
+//    dataType: 'json',
+//    success: function(data) {
+//        var $tr =$('<tr>').addClass('header');
+//        $.each(data.headers, function(i,header){
+//            $tr.append($('<th>').append($('a').addClass('sort').attr('href','#').append($('span').text(header))));
+//        });
+//        $tr.appendTo('table.data');
+//        $.each(data.rows,function(i){
+//            $('<tr>').attr('id',i).
+//                append($('<td>').text(person.name)).
+//                append($('<td>').text(row.company)).
+//                append($('<td>').text(row.location)).appendTo('table.data');
+//        });
+//    }
+//});
+$(document).ready(function() {
+
+	$.getJSON('kandidaadid.json',null , function(json_data){
+	
+	    var table = '<table>';
+	    table += '<tr><th>Nimi</th><th>Piirkond</th><th>Erakond</th><th>H‰‰lte arv</th></tr>';
+	    $.each(json_data, function(index, item){
+	         table += '<tr id="'+item.id+'"><td>'+item.person.name+'</td><td>'+item.region.name+'</td><td>'+item.party.name+'</td><td>'+item.vote_number+'</td></tr>';
+	    })
+	    table +='</table>';
+	    $('#result_table').html(table);
+	})
+})
